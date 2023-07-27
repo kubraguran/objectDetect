779ead70ce74186a71123be45617385ed4cfa173
Kubra Guran <kubraguran00@gmail.com>
2023-07-27 03:36:24 +0200

    detectupdate

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import { drawRect } from "./utilities";
 function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
+  const [noProtection, setNoProtection] = useState(false);
+
 
 
   const runCoco = async () => {
@@ -20,43 +22,52 @@ function App() {
     }, 10);
   };
 
-  const detect = async (net) => {
+ 
 
+  const detect = async (net) => {
     if (
       typeof webcamRef.current !== "undefined" &&
       webcamRef.current !== null &&
       webcamRef.current.video.readyState === 4
     ) {
-
       const video = webcamRef.current.video;
       const videoWidth = webcamRef.current.video.videoWidth;
       const videoHeight = webcamRef.current.video.videoHeight;
-  
-    
+
       webcamRef.current.video.width = videoWidth;
       webcamRef.current.video.height = videoHeight;
-  
 
       canvasRef.current.width = videoWidth;
       canvasRef.current.height = videoHeight;
-  
 
       const obj = await net.detect(video);
-  
-  
+
       const safetyItems = ["helmet", "vest", "gloves", "person"];
-  
 
       const relevantObjects = obj.filter((item) =>
         safetyItems.includes(item.class)
       );
-  
-  
+
+      const persons = relevantObjects.filter((item) => item.class === "person");
+      const personCount = persons.length;
+
       const ctx = canvasRef.current.getContext("2d");
       drawRect(relevantObjects, ctx);
+
+      ctx.font = "24px Arial";
+      ctx.fillStyle = "red";
+      ctx.fillText(`Person Count: ${personCount}`, 10, 30);
+
+      if (relevantObjects.length === 0) {
+        setNoProtection(true);
+        ctx.fillStyle = "yellow";
+        ctx.fillText("Wear Protect", 10, 60);
+      } else {
+        setNoProtection(false);
+      }
     }
   };
-  
+
 
 
   useEffect(()=>{runCoco()},[]);
@@ -94,6 +105,25 @@ function App() {
             height: 480,
           }}
         />
+          {noProtection && (
+          <div
+            style={{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              left: 0,
+              right: 0,
+              textAlign: "center",
+              zIndex: 10,
+              top: "50%",
+              color: "yellow",
+              fontSize: "24px",
+              fontWeight: "bold",
+            }}
+          >
+            Wear Protect
+          </div>
+        )}
       </header>
     </div>
   );
