e9ea1f0d844f5db8d1fefc9dfb7df44541ec9a69
Taehyung Kwon <katalogin88@gmail.com>
2023-08-14 03:53:35 +0100

    Refactor, initiate tailwind css, and revise menu button

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import * as tf from "@tensorflow/tfjs";
-import Offline from "./components/Offline";
+import Offline from "./components/offline";
 import * as cocossd from "@tensorflow-models/coco-ssd";
 import { drawRect } from "./utilities";
-import Live from "./components/Live";
+import Live from "./components/live";
+import Menu from "./components/menu";
+import "./App.css";
 
 function App() {
   return (
     <Router>
-      <div >
-        <div >
-          <button onClick={() => window.location.href = '/live'}>
-            Live Mode
-          </button>
-          <button onClick={() => window.location.href = '/offline'}>
-            Offline Mode
-          </button>
+      <div className="flex flex-row">
+        <Menu />
+        <div>
+          <Routes>
+            <Route path="/live" element={<Live />} />
+            <Route path="/offline" element={<Offline />} />
+          </Routes>
         </div>
-        <Routes>
-          <Route path="/live" element={<Live />} />
-          <Route path="/offline" element={<Offline />} />
-        </Routes>
       </div>
     </Router>
   );
 }
-
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      width: {
+        200: "200px",
+        250: "250px",
+        300: "300px",
+        350: "350px",
+        400: "400px",
+        450: "450px",
+        500: "500px",
+        600: "600px",
+      },
+      height: {
+        200: "200px",
+        250: "250px",
+        300: "300px",
+        350: "350px",
+        400: "400px",
+        450: "450px",
+        500: "500px",
+        600: "600px",
+      },
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+      },
+    },
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+  },
+  plugins: [],
+};
